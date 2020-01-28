6ff3cdc20d964ad3f26e665e8f4b6ceadd0ad282
sbaibos <sbaibos@gmail.com>
2020-01-28 12:09:37 +0200

    gridapi can now update multiple rows from multiple tables

--- objects/updateStock.php
+++ objects/updateStock.php
@@ -13,18 +13,35 @@ $stockmarket = new stockmarket($database->getConnection());
 $data = json_decode(file_get_contents("php://input")); //array of objects
 // get object from array 
 $object = $data[0];
-var_dump($data);
-$stockmarket->name = $object->name;
-$stockmarket->date = $object->date;
-$stockmarket->open = $object->open;
-$stockmarket->high = $object->high;
-$stockmarket->low = $object->low;
-$stockmarket->close = $object->close;
-$stockmarket->volume = $object->volume;
-$stockmarket->adj_high = $object->adj_high;
-$stockmarket->adj_low = $object->adj_low;
-$stockmarket->adj_volume = $object->adj_volume;
-$stockmarket->id=$object->id;
+//for ($i=0;$i<=11;$i++){
+	 foreach($data as $key => $value) {
+		
+$stockmarket->name = $value->name;
+$stockmarket->date = $value->date;
+$stockmarket->open = $value->open;
+$stockmarket->high = $value->high;
+$stockmarket->low = $value->low;
+$stockmarket->close = $value->close;
+$stockmarket->volume = $value->volume;
+$stockmarket->adj_high = $value->adj_high;
+$stockmarket->adj_low = $value->adj_low;
+$stockmarket->adj_volume = $value->adj_volume;
+$stockmarket->id=$value->id;
+		 
+$stockmarket->updateStock()	;	 
+}
+//var_dump($object);
+// $stockmarket->name = $data[0]->name;
+// $stockmarket->date = $data[0]->date;
+// $stockmarket->open = $data[0]->open;
+// $stockmarket->high = $data[0]->high;
+// $stockmarket->low = $data[0]->low;
+// $stockmarket->close = $data[0]->close;
+// $stockmarket->volume = $data[0]->volume;
+// $stockmarket->adj_high = $data[0]->adj_high;
+// $stockmarket->adj_low = $data[0]->adj_low;
+// $stockmarket->adj_volume = $data[0]->adj_volume;
+// $stockmarket->id=$object->id;
 
 
 if ($stockmarket->updateStock()) {
